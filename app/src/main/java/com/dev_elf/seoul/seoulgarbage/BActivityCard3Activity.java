8eceea7ab4a90bbc049a9b5ff915f840107b3875
YURIM <ikisee@naver.com>
2016-09-04 18:09:54 +0900

    add card view

---
+++ app/src/main/java/com/dev_elf/seoul/seoulgarbage/BActivityCard3Activity.java
@@ -0,0 +1,13 @@
+package com.dev_elf.seoul.seoulgarbage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BActivityCard3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.b_activity_card3);
+    }
+}
