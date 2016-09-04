2643acfeaca25a1f2fcd6afa96852611525b927d
geusan <dnay2k@naver.com>
2016-09-04 15:42:22 +0900

    create project

--- app/src/main/java/com/dev_elf/seoul/seoulgarbage/MainActivity.java
+++ app/src/main/java/com/dev_elf/seoul/seoulgarbage/MainActivity.java
@@ -3,6 +3,9 @@ package com.dev_elf.seoul.seoulgarbage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * New app
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
